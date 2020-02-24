71540e648883ac9ddd018d752f27dc724f3b90f6
ddeluc <56974496+ddeluc@users.noreply.github.com>
2020-02-24 09:29:33 -0500

    RPG Blueprint
    Tutorial from Brackeys

---
+++ RPG Project/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+// Comment
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public Interactable focus;
+
+    // Layer that is walkable
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        // Comment
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (Input.GetMouseButton(0))
+        {
+            // Getting the point the user clicks on through the camera
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            // RaycastHit is wherever the last raycast hit
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                // Get the transform of the point that was hit by the raycast on the movement mask (ground)
+                motor.MoveToPoint(hit.point);
+
+                RemoveFocus();
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // Get the interactable component for the collider that was hit
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                // Make sure that it has an interactable compoenent
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+
+
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        // Make sure that the focus is not already in newFocus
+        if (newFocus != focus)
+        {
+            
+            if (focus != null)
+                focus.OnDefocused();
+
+            focus = newFocus;
+            // Follow the focused interactable
+            motor.FollowTarget(newFocus);
+        }
+        
+        newFocus.OnFocused(transform);
+    }
+
+   void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+
+}
---
+++ RPG Project/Assets/Scripts/Stats/Stat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The fields of this clss will show in the inspector
+[System.Serializable]
+public class Stat
+{
+
+    [SerializeField]
+    private int baseValue;
+    // Starting value for given stat
+
+    private List<int> modifiers = new List<int>();
+
+    public int GetValue()
+    {
+        int finalValue = baseValue;
+        modifiers.ForEach(x => finalValue += x);
+        return finalValue;
+    }
+
+    public void AddModifier (int modifier)
+    {
+        if (modifier != 0)
+            modifiers.Add(modifier);
+    }
+
+    public void RemoveModifier (int modifier)
+    {
+        if (modifier != 0)
+            modifiers.Remove(modifier);
+    }
+}
